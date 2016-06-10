2f519fdee6fe7f5807cf67b4cff45966ba2ca0ce
Carter Cole <carter@hvst.com>
2016-06-10 09:40:30 -0500

    add gin index test

--- tests/blog4.2/run-all.sh
+++ tests/blog4.2/run-all.sh
@@ -3,8 +3,8 @@
 mkdir -p db
 
 dropdb db_evolve_test
-echo "CREATE USER db_evolve_test WITH PASSWORD 'password';" | psql template1
-echo "CREATE USER db_evolve_test2 WITH PASSWORD 'password';" | psql template1
+#echo "CREATE USER db_evolve_test WITH PASSWORD 'password';" | psql template1
+#echo "CREATE USER db_evolve_test2 WITH PASSWORD 'password';" | psql template1
 
 set -e
 
---
+++ tests/blog4.2/schemas/schema14.rb
@@ -0,0 +1,30 @@
+DB::Evolve::Schema.define do
+
+  grant :all
+
+  create_table "blog1" do |t|
+    t.string   "col1"
+  end
+
+  create_table "blog2" do |t|
+    t.string   "col1"
+    t.string   "col2"
+    t.integer  "blog1_id"
+  end
+
+  create_table "blog33", :aka => "blog3" do |t|
+    t.string   "col1"
+    t.integer  "blog1_id"
+  end
+
+  add_foreign_key :blog2, :blog1, column: :blog1_id, primary_key: :id
+  add_foreign_key :blog33, :blog1
+
+  create_table "blog44", :aka => "blog4" do |t|
+    t.string   "col2", array: true
+  end
+
+  add_index "blog44", ["col2"], :name => "index_blog44_on_col2", :using => :gin
+
+end
+
